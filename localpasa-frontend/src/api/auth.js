f102e2d3a3e5f9c40415c9cc124c9548ed46a9f7
rezen <rezen.khadgi@gmail.com>
2026-05-04 10:27:24 +0545

    minor error fix

--- localpasa-frontend/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import client from "./client";
-
-export const register = (data) => client.post("/auth/register", data);
-export const login = (data) => client.post("/auth/login", data);
-export const refreshTokens = (refreshToken) =>
-  client.post("/auth/refresh", { refreshToken });
-export const getme = () => client.get("/auth/me");
-export const logout = (refreshToken) =>
-  client.post("/auth/logout", { refreshToken });
